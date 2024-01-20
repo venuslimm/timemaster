fc1c815e9d3ee4f98b261e5e65ca0c8edf13319e
venuslimm <limjiayivenus@gmail.com>
2024-01-20 15:48:57 +0800

    Create skeleton

---
+++ popup.js
@@ -0,0 +1,11 @@
+const contentContainer = document.getElementById('content-container');
+
+document.getElementById('toggleTimer').addEventListener('click', () => {
+  // TODO
+  contentContainer.innerHTML = '<p>1</p>';
+});
+
+document.getElementById('toggleStopwatch').addEventListener('click', () => {
+  // TODO
+  contentContainer.innerHTML = '<p>2</p>';
+});
